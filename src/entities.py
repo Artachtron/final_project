5e1495201b4ad35df73fd5873b4229b9e16a9ae5
Artagon <florent.vaucher@gmail.com>
2022-05-06 06:14:53 +0200

    Correctly place animal entity on grid

--- src/entities.py
+++ src/entities.py
@@ -1,8 +1,17 @@
 import pygame as pg
+from os.path import dirname, realpath, join
+from pathlib import Path
+from collections import namedtuple
+import world
+
+Position = namedtuple('Position', ['x', 'y'])
 
 class Animal(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, size=10, pos_x=0, pos_y=0):
         super().__init__()
-        self.image = pg.image.load(r"H:\UoL\Semester 5\Code\final_project\assets\Fichier 1.png").convert()
-        self.rect = self.image.get_rect()
+        self.size = size
+        image = pg.image.load(join(Path(dirname(realpath(__file__))).parent.absolute(),r'assets\Fichier 1.png')).convert_alpha()
+        self.image = pg.transform.scale(image, (size,size))
+        self.position = Position(pos_x*world.BLOCK_SIZE, pos_y*world.BLOCK_SIZE)
+        self.rect = self.image.get_rect(center= (self.position.x+world.BLOCK_SIZE/2,self.position.y+world.BLOCK_SIZE/2))
         
