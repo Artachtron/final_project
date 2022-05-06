f257bd26b8d594ee67fa40b88546e72313cd67ba
Artagon <florent.vaucher@gmail.com>
2022-05-06 08:40:58 +0200

    numpy array with empty or full cells

--- src/entities.py
+++ src/entities.py
@@ -2,9 +2,12 @@ from tkinter import RIGHT
 import pygame as pg
 from os.path import dirname, realpath, join
 from pathlib import Path
+from typing import Tuple
 import enum
 import random
+import operator
 import world
+
 
 assets_path = join(Path(dirname(realpath(__file__))).parent.absolute(), "assets")
 
@@ -16,39 +19,55 @@ class Direction(enum.Enum):
     
 
 class Animal(pg.sprite.Sprite):
-    def __init__(self, size=10, pos_x=10, pos_y=10):
+    def __init__(self, grid: world.Grid, size: int=20, position: Tuple[int,int]=(int(world.GRID_WIDTH/2),int(world.GRID_HEIGHT/2))):
         super().__init__()
         self.size = size
         image = pg.image.load(join(assets_path,'Animal.png')).convert_alpha()
         self.image = pg.transform.scale(image, (size,size))
-        position_x = pos_x * world.BLOCK_SIZE
-        position_y = pos_y * world.BLOCK_SIZE
-        self.rect = self.image.get_rect(center= (position_x + world.BLOCK_SIZE/2, position_y + world.BLOCK_SIZE/2))
+        self.position = position
+        pos_x, pos_y = self.position
+        self.rect = self.image.get_rect(center= (pos_x * world.BLOCK_SIZE + world.BLOCK_SIZE/2, pos_y * world.BLOCK_SIZE + world.BLOCK_SIZE/2))
+        self.grid = grid
         
     """ def tmp_input(self):
         keys = pg.key.get_pressed()
         if keys[pg.K_UP]:
             pass """
         
-    def move(self, direction: Direction):
-        self.rect.x += direction.value[0] * world.BLOCK_SIZE
-        self.rect.y += direction.value[1] * world.BLOCK_SIZE
+    def move(self, direction: Direction) -> None:
+        next_move = tuple(map(operator.add, self.position, direction.value))
+        
+        if self._check_next_move(next_move):
+            self.grid.update_grid(position=(self.position), value=0)
+            self.position = next_move
+                   
+            self.rect.x = next_move[0]  * world.BLOCK_SIZE
+            self.rect.y = next_move[1]  * world.BLOCK_SIZE
+            
+    def _check_next_move(self, next_move: Tuple[int,int]):
+        return self._check_out_of_bounds(next_move)
+     
+    def _check_out_of_bounds(self, next_move: Tuple[int,int]):
+        next_x, next_y = next_move
+        if next_x < 0 or next_x >= world.GRID_WIDTH or next_y < 0 or next_y >= world.GRID_HEIGHT:
+            return False
+        return True
         
     def update(self):
         direction = random.choice(list(Direction))
-        print(direction)
+        #print(direction)
         self.move(direction)
         
-class Plant(pg.sprite.Sprite):
-    def __init__(self, size=10, pos_x=10, pos_y=10):
+class Tree(pg.sprite.Sprite):
+    def __init__(self, grid:world.Grid, size=20, position: Tuple[int,int]=(int(world.GRID_WIDTH/2),int(world.GRID_HEIGHT/2))):
         super().__init__()
         self.size = size
         image = pg.image.load(join(assets_path,'Plant.png')).convert_alpha()
         self.image = pg.transform.scale(image, (size,size))
-        position_x = pos_x * world.BLOCK_SIZE
-        position_y = pos_y * world.BLOCK_SIZE
-        self.rect = self.image.get_rect(center= (position_x + world.BLOCK_SIZE/2, position_y + world.BLOCK_SIZE/2))
-        
+        self.position = position
+        pos_x, pos_y = self.position
+        self.rect = self.image.get_rect(center= (pos_x * world.BLOCK_SIZE + world.BLOCK_SIZE/2, pos_y * world.BLOCK_SIZE + world.BLOCK_SIZE/2))
+        self.grid = grid
 
         
  
