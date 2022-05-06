c247e96a1e38955fb4a48409f33bf4dff164db18
Artagon <florent.vaucher@gmail.com>
2022-05-06 06:42:54 +0200

    Move animal every 50 ticks

--- src/entities.py
+++ src/entities.py
@@ -4,14 +4,23 @@ from pathlib import Path
 from collections import namedtuple
 import world
 
-Position = namedtuple('Position', ['x', 'y'])
+assets_path = join(Path(dirname(realpath(__file__))).parent.absolute(), "assets")
 
 class Animal(pg.sprite.Sprite):
     def __init__(self, size=10, pos_x=0, pos_y=0):
         super().__init__()
         self.size = size
-        image = pg.image.load(join(Path(dirname(realpath(__file__))).parent.absolute(),r'assets\Fichier 1.png')).convert_alpha()
+        image = pg.image.load(join(assets_path,'Fichier 1.png')).convert_alpha()
         self.image = pg.transform.scale(image, (size,size))
-        self.position = Position(pos_x*world.BLOCK_SIZE, pos_y*world.BLOCK_SIZE)
-        self.rect = self.image.get_rect(center= (self.position.x+world.BLOCK_SIZE/2,self.position.y+world.BLOCK_SIZE/2))
+        position_x = pos_x * world.BLOCK_SIZE
+        position_y = pos_y * world.BLOCK_SIZE
+        self.rect = self.image.get_rect(center= (position_x + world.BLOCK_SIZE/2, position_y + world.BLOCK_SIZE/2))
         
+    def tmp_input(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_UP]:
+            pass
+        
+    def update(self):
+        self.rect.x += 1*world.BLOCK_SIZE
+
--- src/world.py
+++ src/world.py
@@ -14,7 +14,7 @@ WINDOW_WIDTH = BLOCK_SIZE * GRID_WIDTH
 
 
 def main():
-    global SCREEN, CLOCK
+    global SCREEN, CLOCK, counter
     
     pg.init()
     SCREEN = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -23,6 +23,8 @@ def main():
     
     animal_group = pg.sprite.Group()
     animal_group.add(entities.Animal())
+    
+    counter = 0
 
     while True:
         drawGrid()
@@ -33,15 +35,21 @@ def main():
                 sys.exit()
 
         pg.display.update()
-
+        CLOCK.tick(60)
+        counter += 1
+        if counter == 50:
+            counter = 0
 
 def drawGrid():
+    SCREEN.fill(WHITE)
     for x in range(0, WINDOW_WIDTH, BLOCK_SIZE):
         for y in range(0, WINDOW_HEIGHT, BLOCK_SIZE):
             rect = pg.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
             pg.draw.rect(SCREEN, BLACK, rect, 1)
 
 def drawEntities(animal_group):
+    if counter == 0:
+        animal_group.update()    
     animal_group.draw(SCREEN)
 
 if __name__ == "__main__":
