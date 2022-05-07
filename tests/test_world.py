1de36c69c880693b13dbd4147147c08e3522f74a
Artagon <florent.vaucher@gmail.com>
2022-05-07 10:18:43 +0200

    Add tests for grid

--- tests/test_world.py
+++ tests/test_world.py
@@ -12,3 +12,17 @@ class TestGrid:
         assert self.grid
         assert type(self.grid) == world.Grid
         
+    def test_dimensions_grid(self):
+        grid = world.Grid(width=5, height=10)
+        assert grid._width == 5
+        assert grid._height == 10
+        assert grid.dimensions == (5,10)
+        
+    def test_update_cell(self):
+        assert self.grid.get_position_value(position=(5,5)) == 0
+        self.grid.update_grid_cell_value(position=(5,5), value=1)
+        assert self.grid.get_position_value(position=(5,5)) == 1
+        self.grid.update_grid_cell_value(position=(5,5), value=0)
+        assert self.grid.get_position_value(position=(5,5)) == 0
+        
+    
