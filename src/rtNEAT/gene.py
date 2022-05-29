de27fa238eb5ba4bcfd808338ce31202256b8a03
Artagon <florent.vaucher@gmail.com>
2022-05-29 23:28:48 +0200

    Gene class

--- src/rtNEAT/gene.py
+++ src/rtNEAT/gene.py
@@ -1,4 +1,20 @@
+import Node
+
 class Gene:
     def __init__(self,
                  weight: float,
-                 
+                 in_node: Node,
+                 out_node: Node,
+                 recurrence: bool,
+                 innovation_number: int,
+                 mutation_number: int):
+        
+        self.link = Link(weight=weight,
+                    in_node=in_node,
+                    out_node=out_node,
+                    recurrence=recurrence)
+        
+        self.innovation_number: int = innovation_number
+        self.mutation_number: int = mutation_number # Used to see how much mutation has changed the link
+        self.enable = True # When this is off the Gene is disabled
+        self.frozen = False # When frozen, the linkweight cannot be mutated
