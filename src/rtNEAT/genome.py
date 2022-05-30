e1cd54d6337b7feef6cd6e2194a4f0ec9ba44ff3
Artagon <florent.vaucher@gmail.com>
2022-05-30 22:15:09 +0200

    Population class

--- src/rtNEAT/genome.py
+++ src/rtNEAT/genome.py
@@ -9,4 +9,57 @@ class Genome:
         self.id: int = genome_id
         self.nodes: np.array = nodes # List of Nodes for the Network
         self.genes: np.array = genes # List of innovation-tracking genes
+        self.size = len(genes)
+   
+def compatibility(self, genome: Genome):
+    p1_innovation: int
+    p2_innovation: int
+    
+    mutation_difference: float
+    
+    number_disjoint: int
+    number_excess: int = 0
+    mutation_difference_total: float
+    number_matching: int = 0
+    
+    max_genome_size: int = max(self.size, genome.size)
+
+    p1_genome = iter(self.genes)
+    p2_genome = iter(genome.genes)
+    
+    p1_size = self.size
+    p2_size = genome.size
+    p1_gene = next(p1_genome)
+    p2_gene = next(p2_genome)
+    while True:
+        try:
+            if p1_gene == self.genes[-1]:
+                p2_gene = next(p2_genome)
+                number_excess += 1
+            elif p2_gene == genome.genes[-1]:
+                p1_gene = next(p1_genome)
+                number_excess += 1
+            else:
+                p1_innovation = p1_gene.innovation_number
+                p2_innovation = p2_gene.innovation_number
                 
+                if(p1_innovation==p2_innovation):
+                    number_matching += 1
+                    mutation_difference = abs(p1_gene.mutation_number - p2_gene.mutation_number)
+                    mutation_difference_total += mutation_difference
+                    
+                    p1_gene = next(p1_genome)
+                    p2_gene = next(p1_genome)
+                elif p1_innovation < p2_innovation:
+                    p1_gene = next(p1_genome)
+                    number_disjoint += 1
+                elif p2_innovation < p1_innovation:
+                    p2_gene = next(p2_genome)
+                    number_disjoint += 1
+        except StopIteration:
+            pass
+    
+    return (NEAT.disjoint_coeff(number_disjoint) +
+            NEAT.excess_coeff(num_excess) +
+            NEAT.mutation_difference_total(mutation_difference_total(number_matching)))
+                 
---
+++ src/rtNEAT/population.py
@@ -0,0 +1,49 @@
+import numpy as np
+from species import Species
+
+class Population:
+    def __init__(self,
+                 organisms: np.array,
+                 species: np.array,
+                 size: int):
+        self.organisms = organisms
+        self.species = species
+    
+    def speciate(self):
+        comparison_organism = 0
+                
+        counter: int = 0
+        
+        # For each organism, search for a species it is compatible to
+        for current_organism in self.organisms:
+            if len(self.species) == 0:
+                counter += 1
+                new_species = Species(counter)
+                self.species.append(new_species)
+                new_species.add_organism(current_organism) # Add the current organism
+                current_organism.species = new_species # Point organism to its species
+            else:
+                comparison_organism = current_species[0]
+                for current_species in self.species:
+                    if comparison_organism == 0:
+                        break
+                    
+                    if current_organism.genome.compatibility(comparison_organism.genome) < NEAT.threshold:
+                        # Found compatible species, so add this organism to it
+                        current_species.add_organism(current_organism)
+                        current_organism.species = current_species
+                        comparison_organism = 0
+                    else:
+                        # Keep searching for a matching species
+                        current_species += 1
+                        if current_species != len(self.species) - 1:
+                            comparison_organism = current_species[0]
+                            
+                # If we didn't find a match, create a new species
+                if comparison_organism !=0:
+                    counter += 1
+                    new_species = Species(counter)    
+                    self.species.append(new_species)
+                    new_species.add_organism(current_organism) # Add the current organism
+                    current_organism.species = new_species # Point organism to its species
+                
