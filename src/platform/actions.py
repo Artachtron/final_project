b3d7d69ed5ef3ce74b07d7a34ea8d956b8f2b276
Artagon <florent.vaucher@gmail.com>
2022-07-13 18:52:42 +0200

    Tests for actions

--- src/platform/actions.py
+++ src/platform/actions.py
@@ -1,4 +1,3 @@
-
 from abc import ABC
 from dataclasses import dataclass
 from enum import Enum, auto
@@ -20,86 +19,85 @@ class ActionType(Enum):
     PLANT_TREE = auto()
     GROW = auto()
     REPRODUCE = auto()
+    IDLE = auto()
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class Action(ABC):
     """Abstract class:
         Contains necessary information for an action
     """
     action_type: ActionType
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class SelfAction(Action):
     """Superclass:
         Actions that do not modify the environment
     """
-    new_status: Optional[Status]
+    new_status: Optional[Status] = None
 
-
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class Interaction(Action):
     """Superclass:
         Actions that modify the environment
     """
     coordinates: Tuple[int, int]
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class ReproduceAction(SelfAction):
     """SelfAction:
         Action on decision to reproduce
     """
-    new_status = Status.FERTILE
-    action_type = ActionType.REPRODUCE
+    action_type: ActionType = ActionType.REPRODUCE
+    new_status: Status = Status.FERTILE
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class GrowAction(SelfAction):
     """SelfAction:
         Action on decision to grow
     """
-    action_type = ActionType.GROW
+    action_type: ActionType = ActionType.GROW
 
-@dataclass
-class PlantTree(Interaction):
+@dataclass(kw_only=True, frozen=True)
+class PlantTreeAction(Interaction):
     """Interaction:
         Action on decision to plant a tree
     """
-    action_type = ActionType.PLANT_TREE
+    action_type: ActionType = ActionType.PLANT_TREE
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class RecycleAction(Interaction):
     """Interaction:
         Action on decision to recycle a seed
     """
-    action_type = ActionType.RECYCLE
+    action_type: ActionType = ActionType.RECYCLE
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class PickupAction(Interaction):
     """Interaction:
         Action on decision to pick up resource
     """
-    action_type = ActionType.PICKUP
+    action_type: ActionType = ActionType.PICKUP
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class DropAction(Interaction):
     """Interaction:
         Action on decision to drop resource
     """
-    action_type = ActionType.DROP
+    action_type: ActionType = ActionType.DROP
     energy_type: EnergyType
     quantity: int
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class PaintAction(Interaction):
     """Interaction:
         Action on decision to modify a cell's color
     """
-    action_type = ActionType.PAINT
+    action_type: ActionType = ActionType.PAINT
     color: Tuple[int, int, int]
 
-@dataclass
+@dataclass(kw_only=True, frozen=True)
 class MoveAction(Interaction):
     """Interaction:
         Action on decision to move
     """
-    action_type = ActionType.MOVE
-
+    action_type: ActionType = ActionType.MOVE
