c8f279ead858622ec5a712eeed84beb1724bf434
Artagon <florent.vaucher@gmail.com>
2022-08-21 19:49:30 +0200

    launcher running

--- src/platform/running/launcher.py
+++ src/platform/running/launcher.py
@@ -12,10 +12,14 @@ directory = join(
             "configuration/")
 
 files = [f for f in listdir(directory) if isfile(join(directory, f))]
-
+start = False
 for n, f in enumerate(files):
     for i in range(0, 5):
-        print(f"{n} file: {f} {i}")
-        os.system(f"python -m src.platform.running.main --c {f}")
+        if start:
+            print(f"{n} file: {f} {i}")
+            os.system(f"python -m src.platform.running.main --c {f}")
+            
+        if f == 'config_growth_energy_required_10.json':
+            start = True
 #subprocess.call([sys.executable, 'src/platform/running/main.py', '--c config_A_1.json'])
 #subprocess.run("python -m src.platform.running.main --c config_A_1.json",env={"PATH": "H:/UoL/Semester 5/Code/final-project"})
