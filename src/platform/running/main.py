47cbef61083693960ba7e42694b9f1ea9db0c395
Artagon <florent.vaucher@gmail.com>
2022-09-05 02:27:20 +0200

    Requirements.tyt

--- src/platform/running/main.py
+++ src/platform/running/main.py
@@ -6,12 +6,7 @@ from time import time
 from ..world import World
 from .config import config
 
-# py-spy record -o profile.svg --subprocesses -- python -m src.platform.main
-# python -m cProfile -m src.platform.main
-# python -m src.platform.main
-# python -m src.platform.running.main --c config_A_5.json
-# snakeviz ./profile.prof
-
+# python -m src.platform.running.main --c best_config.json -d
 
 def main():
     start = time()
